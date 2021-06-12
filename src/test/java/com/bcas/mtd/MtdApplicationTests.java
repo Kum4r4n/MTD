f82535f8a3e913167ed46e5a145e97a04594a30b
Nisho Kumaran <nishokumaran4@outlook.com>
2021-06-12 22:29:05 +0530

    structure

---
+++ src/test/java/com/bcas/mtd/MtdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcas.mtd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
